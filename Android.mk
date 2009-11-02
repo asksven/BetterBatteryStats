2d70dc1ce88f1eb2511d4f16f774572e4ed3795c
Adam Shanks <adam@noshufou.(none)>
2009-11-01 21:37:20 -0600

    Fix Android.mk to put su-Superuser into /system/bin

--- Android.mk
+++ Android.mk
@@ -2,10 +2,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := su-Superuser
-LOCAL_MODULE_STEM := su
 LOCAL_SRC_FILES := su.c activity.cpp
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := user
 
 LOCAL_C_INCLUDES += external/sqlite/dist
