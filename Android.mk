2998df6bcbc08d8ee6f8d0ba2ad8dd9a803aa421
Koushik K. Dutta <koushd@gmail.com>
2010-02-12 10:34:08 -0800

    Place su in /system/xbin/su. This is where the eng version of su is located. For backwards compatibility, create a symlink for su in /system/bin/su that points to the proper location of su.

--- Android.mk
+++ Android.mk
@@ -16,10 +16,29 @@ LOCAL_CFLAGS += -DUNSIGNED_PACKAGE=1
 endif
 
 
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_OVERRIDES_PACKAGES := su
 PACKAGES.$(LOCAL_MODULE).OVERRIDES := su
 
 include $(BUILD_EXECUTABLE)
+
+# Make #!/system/bin/toolbox launchers for each tool.
+#
+SYMLINK := $(TARGET_OUT_EXECUTABLES)/$(LOCAL_MODULE_STEM)
+$(SYMLINK): SU_BINARY := ../xbin/$(LOCAL_MODULE_STEM)
+$(SYMLINK): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(SU_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf $(SU_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINK)
+
+# We need this so that the installed files could be picked up based on the
+# local module name
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
+    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINK)
+
 
 ## 
 
