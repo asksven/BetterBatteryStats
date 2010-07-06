f4b80cea34b63901cee3f0a0606d20d834efc65d
Daniel Velazco <velazcod@gmail.com>
2010-07-06 17:12:37 -0400

    Show Superuser & su version on preference screen

--- src/com/noshufou/android/su/SuPreferences.java
+++ src/com/noshufou/android/su/SuPreferences.java
@@ -1,6 +1,14 @@
 package com.noshufou.android.su;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 public class SuPreferences extends PreferenceActivity {
@@ -9,5 +17,43 @@ public class SuPreferences extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        
+        Preference versionPreference = (Preference)getPreferenceScreen().findPreference("preference_version");
+        
+        versionPreference.setTitle(getString(R.string.preference_version_title, getSuperuserVersion()));
+        versionPreference.setSummary(getString(R.string.preference_version_summary, getSuVersion()));
+    }
+    
+    private String getSuperuserVersion()
+    {
+    	String versionName = "";
+    	
+    	try
+        {
+        	PackageInfo pInfo = getPackageManager().getPackageInfo("com.noshufou.android.su", PackageManager.GET_META_DATA);
+        	versionName = pInfo.versionName;
+        } catch (NameNotFoundException e)
+        {
+        	e.printStackTrace();
+        }
+        
+        return versionName;
+    }
+    
+    private String getSuVersion()
+    {
+    	String suVersion = "";
+    	Process process = null;
+    	
+	    try {
+			process = Runtime.getRuntime().exec("su -v");
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			suVersion = stdInput.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+    	
+    	return suVersion;
     }
 }
