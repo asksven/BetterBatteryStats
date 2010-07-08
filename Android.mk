733b89fb35f1fa2c5700fa993c317ad9b5139d06
Koushik Dutta <koushd@gmail.com>
2010-07-07 18:27:28 -0700

    LOCAL_MODULE_TAG is not needed for packages.

--- Android.mk
+++ Android.mk
@@ -47,8 +47,6 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
-LOCAL_MODULE_TAGS := user
-
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
