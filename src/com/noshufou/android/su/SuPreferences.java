e1171db3ea575e9c9ad9b6aeaa03696d97fc289a
ChainsDD <chainsdd@gmail.com>
2010-08-22 18:25:50 -0500

    Put su binary version in its own block on the preference screen.
    Also assigned a click listener to check for updates. I had to remove some strings from other localizations because the meanings changed.

--- src/com/noshufou/android/su/SuPreferences.java
+++ src/com/noshufou/android/su/SuPreferences.java
@@ -8,8 +8,11 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.widget.Toast;
 
-public class SuPreferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+public class SuPreferences extends PreferenceActivity implements OnSharedPreferenceChangeListener,
+        OnPreferenceClickListener {
 //	private static final String TAG = "Su.SuPreferences";
 
     @Override
@@ -17,11 +20,14 @@ public class SuPreferences extends PreferenceActivity implements OnSharedPrefere
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
-        Preference versionPreference = (Preference)getPreferenceScreen().findPreference("pref_version");
+        Preference versionPreference = getPreferenceScreen().findPreference("pref_version");
         versionPreference.setTitle(getString(R.string.pref_version_title, getSuperuserVersion()));
         DBHelper db = new DBHelper(this);
-        versionPreference.setSummary(getString(R.string.pref_version_summary, Su.getSuVersion(), db.getDBVersion()));
+        versionPreference.setSummary(getString(R.string.pref_version_summary, db.getDBVersion()));
         db.close();
+        Preference binVersionPreference = getPreferenceScreen().findPreference("pref_bin_version");
+        binVersionPreference.setTitle(getString(R.string.pref_bin_version_title, Su.getSuVersion()));
+        binVersionPreference.setOnPreferenceClickListener(this);
     }
     
 	@Override
@@ -46,7 +52,18 @@ public class SuPreferences extends PreferenceActivity implements OnSharedPrefere
 		}
 	}
 
-	private String getSuperuserVersion()
+	@Override
+    public boolean onPreferenceClick(Preference preference) {
+	    if (preference.getKey().equals("pref_bin_version")) {
+	        Toast.makeText(this, R.string.checking, Toast.LENGTH_SHORT).show();
+	        new Updater(this, Su.getSuVersion()).doUpdate();
+	        return true;
+	    } else {
+	        return false;
+	    }
+    }
+
+    private String getSuperuserVersion()
     {
     	String versionName = "";
     	
