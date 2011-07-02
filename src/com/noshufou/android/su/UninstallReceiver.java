8dee77951d2996e701eda92c627a06227151d043
Adam Shanks <chainsdd@gmail.com>
2011-07-02 07:03:40 +0100

    Remove apps and their associated logs from the database when they're uninstalled

---
+++ src/com/noshufou/android/su/UninstallReceiver.java
@@ -0,0 +1,31 @@
+package com.noshufou.android.su;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+
+import com.noshufou.android.su.provider.PermissionsProvider.Apps;
+
+public class UninstallReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ContentResolver cr = context.getContentResolver();
+        Cursor cursor = cr.query(Apps.CONTENT_URI,
+                new String[] { Apps._ID },
+                Apps.UID + "=?",
+                new String[] { String.valueOf(intent.getIntExtra(Intent.EXTRA_UID, -1)) },
+                null);
+        if (cursor.moveToFirst()) {
+            cr.delete(
+                    ContentUris.withAppendedId(Apps.CONTENT_URI,
+                            cursor.getLong(cursor.getColumnIndex(Apps._ID))),
+                    null, null);
+        }
+        cursor.close();
+    }
+
+}
