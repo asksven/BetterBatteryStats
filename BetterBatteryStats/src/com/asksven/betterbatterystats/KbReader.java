c1c8dd5f7c12a599a56093e3406b16d13964d317
asksven <sven.knispel@gmail.com>
2011-09-16 22:23:41 +0200

    split up class

--- BetterBatteryStats/src/com/asksven/betterbatterystats/KbReader.java
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/KbReader.java
@@ -20,58 +20,21 @@ package com.asksven.betterbatterystats;
  *
  */
 
-import java.util.List;
 import com.google.gson.Gson;
+import com.asksven.betterbatterystats.data.KbData;
+import com.asksven.betterbatterystats.data.SampleKbData;
 
 public class KbReader
 {
-    String json = 
-        "{"
-            + "'title': 'Computing and Information systems',"
-            + "'id' : 1,"
-            + "'children' : 'true',"
-            + "'groups' : [{"
-                + "'title' : 'Level one CIS',"
-                + "'id' : 2,"
-                + "'children' : 'true',"
-                + "'groups' : [{"
-                    + "'title' : 'Intro To Computing and Internet',"
-                    + "'id' : 3,"
-                    + "'children': 'false',"
-                    + "'groups':[]"
-                + "}]" 
-            + "}]"
-        + "}";
-
-    public void main(String... args) throws Exception {
+    
+    public void main(String... args) throws Exception
+    {
 
         // Now do the magic.
-        Data data = new Gson().fromJson(json, Data.class);
+        KbData data = new Gson().fromJson(SampleKbData.json, KbData.class);
 
         // Show it.
         System.out.println(data);
     }
 
-}
-
-class Data
-{
-    private String title;
-    private Long id;
-    private Boolean children;
-    private List<Data> groups;
-
-    public String getTitle() { return title; }
-    public Long getId() { return id; }
-    public Boolean getChildren() { return children; }
-    public List<Data> getGroups() { return groups; }
-
-    public void setTitle(String title) { this.title = title; }
-    public void setId(Long id) { this.id = id; }
-    public void setChildren(Boolean children) { this.children = children; }
-    public void setGroups(List<Data> groups) { this.groups = groups; }
-
-    public String toString() {
-        return String.format("title:%s,id:%d,children:%s,groups:%s", title, id, children, groups);
-    }
 }
