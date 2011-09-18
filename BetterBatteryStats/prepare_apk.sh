4fbc5b8e9951392d262ed13a5394ad1358f886f2
asksven <sven.knispel@gmail.com>
2011-09-18 15:06:33 +0200

    initial commit

---
+++ BetterBatteryStats/prepare_apk.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VERSION=`cat AndroidManifest.xml | grep -o "versionName=\".*\"" | sed 's/"/ /g' | awk {'print $2'}`
+echo "Detected Version $VERSION"
+cp ./bin/BetterBatteryStats.apk ./BetterBatteryStats_xdaedition_$VERSION.apk
