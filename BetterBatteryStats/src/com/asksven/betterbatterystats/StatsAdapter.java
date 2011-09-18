a57e68205dc792fb85a7237a33471042f4b2f946
asksven <sven.knispel@gmail.com>
2011-09-18 15:01:34 +0200

    #6 added all features for KB

--- BetterBatteryStats/src/com/asksven/betterbatterystats/StatsAdapter.java
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/StatsAdapter.java
@@ -18,16 +18,22 @@ package com.asksven.betterbatterystats;
 import java.util.List;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.asksven.android.common.privateapiproxies.StatElement;
+import com.asksven.betterbatterystats.data.KbData;
+import com.asksven.betterbatterystats.data.KbEntry;
 import com.asksven.betterbatterystats.widgets.GraphableButton;
 import com.asksven.betterbatterystats.R;
 
@@ -36,12 +42,20 @@ public class StatsAdapter extends BaseAdapter
     private Context context;
 
     private List<StatElement> m_listData;
+
     private double m_maxValue = 0;
+    
+    /** The Knowlegde base */
+    private KbData m_kb;
 
     public StatsAdapter(Context context, List<StatElement> listData)
     {
         this.context = context;
         this.m_listData = listData;
+
+		// retrieve KB
+		this.m_kb = KbReader.read();
+
         for (int i = 0; i < m_listData.size(); i++)
         {
         	StatElement g = m_listData.get(i);
@@ -76,16 +90,26 @@ public class StatsAdapter extends BaseAdapter
             convertView = inflater.inflate(R.layout.stat_row, null);
         }
         TextView tvName = (TextView) convertView.findViewById(R.id.TextViewName);
-        tvName.setText(entry.getName());
+       	tvName.setText(entry.getName());
 
+        KbEntry kbentry = m_kb.findByStatElement(entry.getName(), entry.getFqn(context));
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.context);
+        boolean bShowKb = sharedPrefs.getBoolean("enable_kb", true);
+        ImageView iconKb = (ImageView) convertView.findViewById(R.id.imageKB);
+        if ( (bShowKb) && (kbentry != null))
+        {
+        	iconKb.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+        	iconKb.setVisibility(View.INVISIBLE);
+        }
         TextView tvFqn = (TextView) convertView.findViewById(R.id.TextViewFqn);
         tvFqn.setText(entry.getFqn(context));
 
         TextView tvData = (TextView) convertView.findViewById(R.id.TextViewData);
         tvData.setText(entry.getData());
-
-        // Only do if prefs are set accordingly
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.context);
         
         LinearLayout myLayout = (LinearLayout) convertView.findViewById(R.id.LinearLayoutBar);
 		
@@ -101,19 +125,52 @@ public class StatsAdapter extends BaseAdapter
         	myLayout.setVisibility(View.VISIBLE);
         	buttonBar.setValues(entry.getValues(), m_maxValue);
         	buttonBar.setHeight(20);
-//        	double[] test = new double[2];
-//        	test[0] = 50;
-//        	double max = 100;
-//        	buttonBar.setValues(test, max);
-        	
+        }
+        
+        // add on click listener only if KB is enabled
+        if (bShowKb)
+        {
+	        // set a click listener for the list
+	        convertView.setOnClickListener(new OnItemClickListener(position));
         }
         return convertView;
     }
-
-    private void showDialog(StatElement entry)
-    {
-        // Create and show your dialog
-        // Depending on the Dialogs button clicks delete it or do nothing
+    
+    private class OnItemClickListener implements OnClickListener
+    {           
+        private int m_iPosition;
+        OnItemClickListener(int position)
+        {
+                m_iPosition = position;
+        }
+        
+        @Override
+        public void onClick(View arg0)
+        {
+        	StatElement entry = (StatElement) getItem(m_iPosition);
+            
+        	KbEntry kbentry = m_kb.findByStatElement(entry.getName(), entry.getFqn(StatsAdapter.this.context));
+  	      	if (kbentry != null)
+  	      	{
+	  	      	SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(StatsAdapter.this.context);
+	  	      	
+	  	      	String url = kbentry.getUrl();
+	  	        if (sharedPrefs.getBoolean("kb_ext_browser", true))
+	  	        {
+					
+					Intent intent = new Intent("android.intent.action.VIEW",
+							Uri.parse(url)); 
+					StatsAdapter.this.context.startActivity(intent);
+	  	        }
+	  	        else
+	  	        {
+		  	      	Intent intentKB = new Intent(StatsAdapter.this.context,
+		  	      			HelpActivity.class);
+		  	      	intentKB.putExtra("url", url);
+		  	        StatsAdapter.this.context.startActivity(intentKB);
+	  	        }           
+  	      	}
+        }
     }
-
 }
+
