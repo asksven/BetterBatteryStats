591559b17aa8191e89e73e4b28bbe4fba62ce1ff
asksven <sven.knispel@gmail.com>
2011-11-05 17:14:34 +0100

    Cleaned up, added menu to more graphs

--- BetterBatteryStats/src/com/asksven/betterbatterystats/BatteryGraphActivity.java
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/BatteryGraphActivity.java
@@ -75,7 +75,7 @@ import java.util.TimerTask;
  * @author sven
  *
  */
-public class BatteryGraphActivity extends Activity // implements OnTouchListener
+public class BatteryGraphActivity extends Activity
 {
 	/**
 	 * a progess dialog to be used for long running tasks
@@ -89,11 +89,7 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
     private XYPlot m_plotScreenOn;
     private XYPlot m_plotWifi;
 
-//    private Viewport m_viewPort; 
-
-//    private XYPlot m_plotCharging;
-
-    private ArrayList<HistoryItem> m_histList;
+    protected static ArrayList<HistoryItem> m_histList;
 	    
     public void onCreate(Bundle savedInstanceState)
     {
@@ -102,7 +98,6 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
         m_plotCharge 	= (XYPlot) findViewById(R.id.myBatteryXYPlot);
         m_plotWakelock 	= (XYPlot) findViewById(R.id.wakelockPlot);
         m_plotScreenOn 	= (XYPlot) findViewById(R.id.screenOnPlot);
-//        m_plotCharging 	= (XYPlot) findViewById(R.id.chargingPlot);
         m_plotWifi	 	= (XYPlot) findViewById(R.id.wifiPlot);
         
 		//new LoadStatData().execute(this);
@@ -111,21 +106,16 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
       
         makePlotPretty(m_plotCharge);
         makePlotPretty(m_plotWakelock);
-//        makePlotPretty(m_plotCharging);
         makePlotPretty(m_plotScreenOn);
         makePlotPretty(m_plotWifi);
         
         refreshPlot(m_plotCharge);
         refreshPlot(m_plotWakelock);
-//        refreshPlot(m_plotCharging);
         refreshPlot(m_plotScreenOn);
         refreshPlot(m_plotWifi);
 
 		//Set of internal variables for keeping track of the boundaries
 		m_plotCharge.calculateMinMaxVals();
-//		m_viewPort = new Viewport(m_plotCharge);
-
-//		m_plotCharge.setOnTouchListener(this);
     }
 	 
 	/* Request updates at startup */
@@ -147,31 +137,6 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
         inflater.inflate(R.menu.history_menu, menu);
         return true;
     }  
-
-    @Override
-	public boolean onPrepareOptionsMenu(Menu menu)
-    {
-//    	boolean bSortingEnabled = true;
-    	    	
-//    	MenuItem zoomIn = menu.findItem(R.id.zoom_plus);
-//    	MenuItem zoomOut = menu.findItem(R.id.zoom_minus);
-    	
-//    	if (m_iSorting == 0)
-//    	{
-//    		// sorting is by time
-//    		sortTime.setEnabled(false);
-//    		sortCount.setEnabled(true);
-//    	}
-//    	else
-//    	{
-//    		// sorting is by count
-//    		sortTime.setEnabled(true);
-//    		sortCount.setEnabled(false);
-//    	}
-//    			
-		return true;
-    	
-    }
 	
     /** 
      * Define menu action
@@ -187,40 +152,30 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
 				Intent intentHist = new Intent(this, HistActivity.class);
 			    this.startActivity(intentHist);
 			    break;
-            case R.id.dump:
+			case R.id.more:
+				Intent intentMore = new Intent(this, BatteryGraph2Activity.class);
+			    this.startActivity(intentMore);
+			    break;
+
+			case R.id.dump:
             	// Dump to File
             	new WriteDumpFile().execute("");
             	break;
-
-//			case R.id.zoom_plus:
-//				m_viewPort.zoom(2);
-//				checkBoundaries();
-//				m_plotCharge.redraw();
-//				break;
-//
-//			case R.id.zoom_minus:
-//				m_viewPort.zoom(0.5f);
-//				checkBoundaries();
-//				m_plotCharge.redraw();
-//				break;
 			case R.id.refresh:
 				m_histList = this.getHistList();
-//				checkBoundaries();
-				m_plotCharge.redraw();
-			break;
-	
-
-        }
-        
+		        refreshPlot(m_plotCharge);
+		        refreshPlot(m_plotWakelock);
+		        refreshPlot(m_plotScreenOn);
+		        refreshPlot(m_plotWifi);
+		        break;
+        }        
         return true;
     }
-			
-
-	    
+			    
      /**
      * Cleans up the plot's general layout and color scheme
      */
-    private void makePlotPretty(XYPlot plot)
+    protected void makePlotPretty(XYPlot plot)
     {	 
     	if (plot != null)
     	{
@@ -298,20 +253,12 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
 		m_plotScreenOn.addSeries(mySerie3, formater2);	        
 		configBinPlot(m_plotScreenOn);
 
-//        // SERIES #4:
-//		BatteryGraphSeries mySerie4 = new BatteryGraphSeries(
-//				m_histList,
-//				BatteryGraphSeries.SERIE_CHARGING,
-//				"Charging");
-//		m_plotCharging.addSeries(mySerie4, formater);	        
-//		configBinPlot(m_plotCharging);
-
-        // SERIES #5:
-		BatteryGraphSeries mySerie5 = new BatteryGraphSeries(
+        // SERIES #4:
+		BatteryGraphSeries mySerie4 = new BatteryGraphSeries(
 				m_histList,
 				BatteryGraphSeries.SERIE_WIFI,
 				"Wifi");
-		m_plotWifi.addSeries(mySerie5, formater2);	        
+		m_plotWifi.addSeries(mySerie4, formater2);	        
 		configBinPlot(m_plotWifi);
 
     }
@@ -320,7 +267,7 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
      * Set common attributes for binary (0/1) plots
      * @param plot the plot to be configured
      */
-    private void configBinPlot(XYPlot plot)
+    protected void configBinPlot(XYPlot plot)
     {
         plot.setTicksPerDomainLabel(2);
         plot.setTicksPerRangeLabel(1);
@@ -337,21 +284,13 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
         plot.getGraphWidget().getDomainOriginLabelPaint().setAlpha(0);
         plot.getGraphWidget().getRangeLabelPaint().setAlpha(0);
         plot.getGraphWidget().getRangeOriginLabelPaint().setAlpha(0);
-        
-        /*
-         * for future version 0.44
-         */
-//        plot.getGraphWidget().setDomainLabelPaint(null);
-//        plot.getGraphWidget().setDomainOriginLabelPaint(null);
-//        plot.getGraphWidget().setRangeLabelPaint(null);
-//        plot.getGraphWidget().setRangeOriginLabelPaint(null);
     }
     
     /**
      * refresh a plot
      * @param plot the plot to be refreshed
      */
-    private void refreshPlot(XYPlot plot)
+    void refreshPlot(XYPlot plot)
     {
     	plot.redraw();
     }
@@ -359,7 +298,7 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
 	 * Get the Stat to be displayed
 	 * @return a List of StatElements sorted (descending)
 	 */
-	private ArrayList<HistoryItem> getHistList()
+	protected ArrayList<HistoryItem> getHistList()
 	{
 		if (AndroidVersion.isFroyo())
 		{
@@ -381,7 +320,7 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
 	}
 
 		
-	private class MyDateFormat extends Format 
+	class MyDateFormat extends Format 
 	{
         // create a simple date format that draws on the year portion of our timestamp.
         // see http://download.oracle.com/javase/1.4.2/docs/api/java/text/SimpleDateFormat.html
@@ -446,174 +385,6 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
 	    }
 	}
 
-	/** handle multitouch
-	 * 
-	 */
-	// Definition of the touch states
-	static final private int NONE = 0;
-	static final private int ONE_FINGER_DRAG = 1;
-	static final private int TWO_FINGERS_DRAG = 2;
-	private int mode = NONE;
- 
-	private PointF firstFinger;
-	private float lastScrolling;
-	private float distBetweenFingers;
-	private float lastZooming;
- 
-//	@Override
-//	public boolean onTouch(View arg0, MotionEvent event)
-//	{
-//		switch(event.getAction() & MotionEvent.ACTION_MASK)
-//		{
-//			case MotionEvent.ACTION_DOWN: // Start gesture
-//				firstFinger = new PointF(event.getX(), event.getY());
-//				mode = ONE_FINGER_DRAG;
-//				break;
-//			case MotionEvent.ACTION_UP:
-//			case MotionEvent.ACTION_POINTER_UP:
-//				//When the gesture ends, a thread is created to give inertia to the scrolling and zoom 
-//				final Timer t = new Timer();
-//				t.schedule(new TimerTask()
-//				{
-//					@Override
-//					public void run()
-//					{
-//						while(Math.abs(lastScrolling) > 1f
-//								|| Math.abs(lastZooming - 1) < 1.01)
-//						{
-//							lastScrolling *= .8;	//speed of scrolling damping
-//							m_viewPort.scroll(lastScrolling);
-//							lastZooming += (1 - lastZooming) * .2;	//speed of zooming damping
-////							m_viewPort.zoom(lastZooming);
-//							checkBoundaries();
-//							try
-//							{
-//								m_plotCharge.postRedraw();
-//							}
-//							catch (final InterruptedException e)
-//							{
-//								e.printStackTrace();
-//							}
-//							// the thread lives until the scrolling and zooming are imperceptible
-//						}
-//					}
-//				}, 0);
-// 
-//			case MotionEvent.ACTION_POINTER_DOWN: // second finger
-//				distBetweenFingers = spacing(event);
-//				// the distance check is done to avoid false alarms
-//				if (distBetweenFingers > 5f)
-//				{
-//					mode = TWO_FINGERS_DRAG;
-//				}
-//				break;
-//			case MotionEvent.ACTION_MOVE:
-//				if (mode == ONE_FINGER_DRAG)
-//				{
-//					final PointF oldFirstFinger = firstFinger;
-//					firstFinger = new PointF(event.getX(), event.getY());
-//					lastScrolling = oldFirstFinger.x - firstFinger.x;
-//					m_viewPort.scroll(lastScrolling);
-//					lastZooming = (firstFinger.y - oldFirstFinger.y) / m_plotCharge.getHeight();
-//					if (lastZooming < 0)
-//					{
-//						lastZooming = 1 / (1 - lastZooming);
-//					}
-//					else
-//					{
-//						lastZooming += 1;
-//					}
-////					m_viewPort.zoom(lastZooming);
-//					checkBoundaries();
-//					m_plotCharge.redraw();
-// 
-//				}
-////				else if (mode == TWO_FINGERS_DRAG) {
-////					final float oldDist = distBetweenFingers;
-////					distBetweenFingers = spacing(event);
-////					lastZooming = oldDist / distBetweenFingers;
-////					m_viewPort.zoom(lastZooming);
-////					checkBoundaries();
-////					mySimpleXYPlot.redraw();
-////				}
-//				break;
-//		}
-//		return true;
-//	}
-// 
-// 
-//	private float spacing(MotionEvent event)
-//	{
-//		final float x = event.getX(0) - event.getX(1);
-//		final float y = event.getY(0) - event.getY(1);
-//		return FloatMath.sqrt(x * x + y * y);
-//	}
-// 
-//	private void checkBoundaries()
-//	{
-//		//Make sure the proposed domain boundaries will not cause plotting issues
-//		m_viewPort.updateBoudaries();
-//		m_plotCharge.setDomainBoundaries(m_viewPort.minXY.x, m_viewPort.maxXY.x, BoundaryMode.AUTO);
-//	}
-//
-//	class Viewport
-//	{
-//		public PointF minXY;
-//		public PointF maxXY;
-//		public float absMinX;
-//		public float absMaxX;
-//		public float minNoError;
-//		public float maxNoError;
-////		public double minDif;
-//		protected double MIN_DIFF;
-//
-//		public Viewport(XYPlot aPlot)
-//		{
-//			minXY = new PointF(m_plotCharge.getCalculatedMinX().floatValue(),
-//					m_plotCharge.getCalculatedMinY().floatValue()); //initial minimum data point
-//			absMinX = minXY.x; //absolute minimum data point
-//			//absolute minimum value for the domain boundary maximum
-//			minNoError = Math.round(m_histList.get(1).getNormalizedTimeLong().floatValue() + 2);
-//			maxXY = new PointF(m_plotCharge.getCalculatedMaxX().floatValue(),
-//					m_plotCharge.getCalculatedMaxY().floatValue()); //initial maximum data point
-//			absMaxX = maxXY.x; //absolute maximum data point
-//			//absolute maximum value for the domain boundary minimum
-//			maxNoError = (float) Math.round(m_histList.get(m_histList.size() - 1).getNormalizedTimeLong().floatValue()) - 2;
-//			MIN_DIFF = (absMaxX - absMinX) / 10;
-//
-//		}
-//		private void zoom(float scale)
-//		{
-//			final float domainSpan = maxXY.x - minXY.x;
-//			final float domainMidPoint = maxXY.x - domainSpan / 2.0f;
-//			final float offset = domainSpan * scale / 2.0f;
-//			minXY.x = domainMidPoint - offset;
-//			maxXY.x = domainMidPoint + offset;
-//		}
-//	 
-//		private void scroll(float pan)
-//		{
-//			final float domainSpan = maxXY.x - minXY.x;
-//			final float step = domainSpan / m_plotCharge.getWidth();
-//			final float offset = pan * step;
-//			minXY.x += offset;
-//			maxXY.x += offset;
-//		}
-//		
-//		private void updateBoudaries()
-//		{
-//			if (minXY.x < absMinX)
-//				minXY.x = absMinX;
-//			else if (minXY.x > maxNoError)
-//				minXY.x = maxNoError;
-//			if (maxXY.x > absMaxX)
-//				maxXY.x = absMaxX;
-//			else if (maxXY.x < minNoError)
-//				maxXY.x = minNoError;
-//			if (maxXY.x - minXY.x < MIN_DIFF)
-//				maxXY.x = maxXY.x + (float) (MIN_DIFF - (maxXY.x - minXY.x));
-//		}
-//	}
 
 	private class WriteDumpFile extends AsyncTask
 	{
@@ -680,7 +451,7 @@ public class BatteryGraphActivity extends Activity // implements OnTouchListener
 			       			+ entry.getGpsOn() + ";"
 			       			+ entry.getWifiRunning() + ";"
 			       			+ entry.getWakelock() + ";"
-			       			+ entry.getBLuetoothOn() + ";"
+			       			+ entry.getBluetoothOn() + ";"
 			       			+ entry.getPhoneInCall() + ";"
 			       			+ entry.getPhoneScanning() 
 			       			+ "\n");	
