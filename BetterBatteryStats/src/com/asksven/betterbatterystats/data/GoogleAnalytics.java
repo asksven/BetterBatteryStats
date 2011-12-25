2c40a7bc726bcc2a6907f0ad4265250a1f0cec99
asksven <sven.knispel@gmail.com>
2011-12-25 18:12:41 +0100

    Fixed #73: [Feature] add google analytics https://github.com/asksven/BetterBatteryStats/issues/issue/73

---
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/data/GoogleAnalytics.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2011 asksven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asksven.betterbatterystats.data;
+
+import android.content.Context;
+
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+
+/**
+ * @author sven
+ *
+ */
+public class GoogleAnalytics
+{
+	private static GoogleAnalytics m_singleton = null;
+	private static GoogleAnalyticsTracker m_tracker = null;
+	
+	public static final String ACTIVITY_ABOUT = "/AboutActivity";
+	public static final String ACTIVITY_ALARMS = "/AlarmsActivity";
+	public static final String ACTIVITY_BATTERY_GRAPH = "/BatteryGraphActivity";
+	public static final String ACTIVITY_BATTERY_GRAPH2 = "/BatteryGraph2Activity";
+	public static final String ACTIVITY_BATTERY_GRAPH_SERIES = "/BatteryGraphSeriesActivity";
+	public static final String ACTIVITY_HELP = "/HelpActivity";
+	public static final String ACTIVITY_HOWTO = "/HowtoActivity";
+	public static final String ACTIVITY_README = "/ReadmeActivity";
+	public static final String ACTIVITY_HIST = "/HistActivity";
+	public static final String ACTIVITY_PREFERENCES = "/PreferencesActivity";
+	public static final String ACTIVITY_STATS = "/StatsActivity";
+	
+	public static final String ACTION_DUMP = "/ActionDump";
+	public static final String ACTION_SET_CUSTOM_REF = "/ActionSetCustmRef";
+	
+
+	private GoogleAnalytics()
+	{
+		
+	}
+	
+	public static GoogleAnalytics getInstance(Context context)
+	{
+		if (m_singleton == null)
+		{
+			m_singleton = new GoogleAnalytics();
+			m_tracker = GoogleAnalyticsTracker.getInstance();
+
+		    // Start the tracker in manual dispatch mode...
+		    m_tracker.startNewSession("TRACKING_CODE_HERE", 20, context);
+		}
+		return m_singleton;
+	}
+	
+	public void trackPage(String page)
+	{
+		m_tracker.trackPageView(page);
+	}
+
+	public void trackStats(String page, int stat, int statType, int sort)
+	{
+		m_tracker.trackPageView(page + "Stat" + stat + "Type" + statType + "Sort" + sort);
+	}
+
+}
