dc11c4b669ba02c72f3423be1f6414957415f149
asksven <sven.knispel@gmail.com>
2011-12-25 18:27:58 +0100

    Detect if tracking is still enabled before tracking anything

--- BetterBatteryStats/src/com/asksven/betterbatterystats/data/GoogleAnalytics.java
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/data/GoogleAnalytics.java
@@ -16,7 +16,13 @@
 package com.asksven.betterbatterystats.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import com.asksven.android.common.utils.DateUtils;
+import com.asksven.betterbatterystats.R;
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
 /**
@@ -27,6 +33,7 @@ public class GoogleAnalytics
 {
 	private static GoogleAnalytics m_singleton = null;
 	private static GoogleAnalyticsTracker m_tracker = null;
+	private static boolean m_bActive = false;
 	
 	public static final String ACTIVITY_ABOUT = "/AboutActivity";
 	public static final String ACTIVITY_ALARMS = "/AlarmsActivity";
@@ -51,25 +58,38 @@ public class GoogleAnalytics
 	
 	public static GoogleAnalytics getInstance(Context context)
 	{
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+		m_bActive = sharedPrefs.getBoolean("use_analytics", true);
+
 		if (m_singleton == null)
 		{
 			m_singleton = new GoogleAnalytics();
-			m_tracker = GoogleAnalyticsTracker.getInstance();
+			if (m_bActive)
+			{
+				m_tracker = GoogleAnalyticsTracker.getInstance();
 
-		    // Start the tracker in manual dispatch mode...
-		    m_tracker.startNewSession("TRACKING_CODE_HERE", 20, context);
+				// Start the tracker in manual dispatch mode...
+				m_tracker.startNewSession("TRACKING_CODE_HERE", 20, context);
+			}
+		    
 		}
 		return m_singleton;
 	}
 	
 	public void trackPage(String page)
 	{
-		m_tracker.trackPageView(page);
+		if ((m_tracker != null) && m_bActive )
+		{
+			m_tracker.trackPageView(page);
+		}
 	}
 
 	public void trackStats(String page, int stat, int statType, int sort)
 	{
-		m_tracker.trackPageView(page + "Stat" + stat + "Type" + statType + "Sort" + sort);
+		if ((m_tracker != null) && m_bActive )
+		{
+			m_tracker.trackPageView(page + "Stat" + stat + "Type" + statType + "Sort" + sort);
+		}
 	}
 
 }
