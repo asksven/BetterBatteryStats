4e0adb5335f20ad956c3890120959e6d63c5ba0c
asksven <sven.knispel@gmail.com>
2012-07-05 21:59:13 +0200

    Added both handling of screen unlock and screen on as triggers for the watchdog

--- BetterBatteryStats/src/com/asksven/betterbatterystats/ScreenEventHandler.java
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/ScreenEventHandler.java
@@ -88,124 +88,136 @@ public class ScreenEventHandler extends BroadcastReceiver
         if (intent.getAction().equals(Intent.ACTION_SCREEN_ON))
 		{
 			Log.i(TAG, "Received Broadcast ACTION_SCREEN_ON");
-			// todo: evaluate what hapened while screen was off here
-			try
+			boolean bRunOnUnlock = sharedPrefs.getBoolean("watchdog_on_unlock", false);
+			
+			if (!bRunOnUnlock)
 			{
-				
-			}
-			catch (Exception e)
-			{
-				Log.e(TAG, "An error occured: " + e.getMessage());
+				processScreenOn(context);
 			}
 
+			
 		}
         
         if (intent.getAction().equals(Intent.ACTION_USER_PRESENT))
 		{
 			Log.i(TAG, "Received Broadcast ACTION_USER_PRESENT");
+			boolean bRunOnUnlock = sharedPrefs.getBoolean("watchdog_on_unlock", false);
 			
-			try
+			if (bRunOnUnlock)
 			{
-				boolean bIssueWarnings = sharedPrefs.getBoolean("watchdog_issue_warnings", false);
-				if (bIssueWarnings)
-				{
-					boolean bShowToast = sharedPrefs.getBoolean("watchdog_show_toasts", false);
-					if (bShowToast)
-					{
-						Toast.makeText(context, "BBS: Watchdog processing...", Toast.LENGTH_SHORT).show();
-					}
-
-					int minScreenOffDurationMin 	= sharedPrefs.getInt("watchdog_duration_threshold", 10);
-					int awakeThresholdPct		= sharedPrefs.getInt("watchdog_awake_threshold", 30);
-					long now = System.currentTimeMillis();
-					Long screenOffTime 			= sharedPrefs.getLong("screen_went_off_at", now );
-					
-					Long screenOffDurationMs 		= now - screenOffTime;
-					
-					// we process only if screenOffDuration is >= minScreenOffDuration
-					if (screenOffDurationMs >= ((long)minScreenOffDurationMin*60*1000))
-					{
-						int awakePct = 0;
-						StatsProvider stats = StatsProvider.getInstance(context);
-						// make sure to flush cache
-						BatteryStatsProxy.getInstance(context).invalidate();
-						
-						if (stats.hasScreenOffRef())
-						{
-							// restore any available since screen reference
-							StatsProvider.getInstance(context).deserializeFromFile();
-							ArrayList<StatElement> otherStats = stats.getOtherUsageStatList(true, StatsProvider.STATS_SCREEN_OFF, false);
-
-							long timeAwake = 0;
-							long timeSince = 0;
-
-							if ( (otherStats != null) && ( otherStats.size() > 1) )
-							{
-								timeAwake = ((Misc) stats.getElementByKey(otherStats, "Awake")).getTimeOn();
-								timeSince = stats.getBatteryRealtime(StatsProvider.STATS_SCREEN_OFF);
-								
-							}
-							else
-							{
-								// no stats means the phone was awake
-								timeSince = stats.getBatteryRealtime(StatsProvider.STATS_SCREEN_OFF);
-								timeAwake = timeSince;
-							}
-							
-							if (timeSince > 0)
-							{
-								awakePct = ((int) (timeAwake / timeSince)) * 100;
-							}
-							else
-							{
-								awakePct = 0;
-							}
-
-							// we issue a warning if awakePct > awakeThresholdPct
-							if (awakePct >= awakeThresholdPct)
-							{
-								if (bShowToast)
-								{
-									Toast.makeText(context, "BBS: Awake alert: " + awakePct + "% awake", Toast.LENGTH_SHORT).show();
-								}
-
-								// notify the user of the situation
-						    	Notification notification = new Notification(
-						    			R.drawable.icon_notext, "Awake alert", System.currentTimeMillis());
-						    	
-								Intent i = new Intent(Intent.ACTION_MAIN);
-								PackageManager manager = context.getPackageManager();
-								
-								i = manager.getLaunchIntentForPackage(context.getPackageName());
-								i.addCategory(Intent.CATEGORY_LAUNCHER);
-							    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-								i.putExtra(StatsActivity.STAT, 0);
-								i.putExtra(StatsActivity.STAT_TYPE, StatsProvider.STATS_SCREEN_OFF);
-								i.putExtra(StatsActivity.FROM_NOTIFICATION, true);
-
-						    	PendingIntent contentIntent = PendingIntent.getActivity(context, 0, i, 0);
-
-						    	notification.setLatestEventInfo(
-						    			context, context.getText(R.string.app_name), 
-						    			awakePct + "% awake while screen off", contentIntent);
-						    	NotificationManager nM = (NotificationManager)context.getSystemService(Service.NOTIFICATION_SERVICE);
-						    	nM.notify(EventWatcherService.NOTFICATION_ID, notification);
-							}
-						}
-					}
-					
-				}
-				
+				processScreenOn(context);
 			}
-			catch (Exception e)
-			{
-				Log.e(TAG, "An error occured: " + e.getMessage());
-			}
+			
 
 		}
 
         Intent i = new Intent(context, EventWatcherService.class);
         context.startService(i);
+    }
+    
+    void processScreenOn(Context context)
+    {
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+		try
+		{
+			boolean bIssueWarnings = sharedPrefs.getBoolean("watchdog_issue_warnings", false);
+			if (bIssueWarnings)
+			{
+				boolean bShowToast = sharedPrefs.getBoolean("watchdog_show_toasts", false);
+
+				int minScreenOffDurationMin 	= sharedPrefs.getInt("watchdog_duration_threshold", 10);
+				int awakeThresholdPct		= sharedPrefs.getInt("watchdog_awake_threshold", 30);
+				long now = System.currentTimeMillis();
+				Long screenOffTime 			= sharedPrefs.getLong("screen_went_off_at", now );
+				
+				Long screenOffDurationMs 		= now - screenOffTime;
+				
+				// we process only if screenOffDuration is >= minScreenOffDuration
+				if (screenOffDurationMs >= ((long)minScreenOffDurationMin*60*1000))
+				{
+					if (bShowToast)
+					{
+						Toast.makeText(context, "BBS: Watchdog processing...", Toast.LENGTH_SHORT).show();
+					}
+
+					int awakePct = 0;
+					StatsProvider stats = StatsProvider.getInstance(context);
+					// make sure to flush cache
+					BatteryStatsProxy.getInstance(context).invalidate();
+					
+					if (stats.hasScreenOffRef())
+					{
+						// restore any available since screen reference
+						StatsProvider.getInstance(context).deserializeFromFile();
+						ArrayList<StatElement> otherStats = stats.getOtherUsageStatList(true, StatsProvider.STATS_SCREEN_OFF, false);
+
+						long timeAwake = 0;
+						long timeSince = 0;
+
+						if ( (otherStats != null) && ( otherStats.size() > 1) )
+						{
+							timeAwake = ((Misc) stats.getElementByKey(otherStats, "Awake")).getTimeOn();
+							timeSince = stats.getBatteryRealtime(StatsProvider.STATS_SCREEN_OFF);
+							
+						}
+						else
+						{
+							// no stats means the phone was awake
+							timeSince = stats.getBatteryRealtime(StatsProvider.STATS_SCREEN_OFF);
+							timeAwake = timeSince;
+						}
+						
+						if (timeSince > 0)
+						{
+							awakePct = ((int) (timeAwake / timeSince)) * 100;
+						}
+						else
+						{
+							awakePct = 0;
+						}
+
+						// we issue a warning if awakePct > awakeThresholdPct
+						if (awakePct >= awakeThresholdPct)
+						{
+							if (bShowToast)
+							{
+								Toast.makeText(context, "BBS: Awake alert: " + awakePct + "% awake", Toast.LENGTH_SHORT).show();
+							}
+
+							// notify the user of the situation
+					    	Notification notification = new Notification(
+					    			R.drawable.icon_notext, "Awake alert", System.currentTimeMillis());
+					    	
+							Intent i = new Intent(Intent.ACTION_MAIN);
+							PackageManager manager = context.getPackageManager();
+							
+							i = manager.getLaunchIntentForPackage(context.getPackageName());
+							i.addCategory(Intent.CATEGORY_LAUNCHER);
+						    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+							i.putExtra(StatsActivity.STAT, 0);
+							i.putExtra(StatsActivity.STAT_TYPE, StatsProvider.STATS_SCREEN_OFF);
+							i.putExtra(StatsActivity.FROM_NOTIFICATION, true);
+
+					    	PendingIntent contentIntent = PendingIntent.getActivity(context, 0, i, 0);
+
+					    	notification.setLatestEventInfo(
+					    			context, context.getText(R.string.app_name), 
+					    			awakePct + "% awake while screen off", contentIntent);
+					    	NotificationManager nM = (NotificationManager)context.getSystemService(Service.NOTIFICATION_SERVICE);
+					    	nM.notify(EventWatcherService.NOTFICATION_ID, notification);
+						}
+					}
+				}
+				
+			}
+			
+		}
+		catch (Exception e)
+		{
+			Log.e(TAG, "An error occured: " + e.getMessage());
+		}
+
     }
 
 }
