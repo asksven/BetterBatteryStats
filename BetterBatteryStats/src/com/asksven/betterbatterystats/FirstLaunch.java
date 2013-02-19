2342f396803a68b4532a168f237f817f4ffe205a
asksven <sven.knispel@gmail.com>
2013-02-19 22:36:26 +0100

    Fixed - task 346: [feature] save 'since unplugged' ref at first use to avoid first time users to have empty screens https://github.com/asksven/BetterBatteryStats/issues/issue/346

---
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/FirstLaunch.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package com.asksven.betterbatterystats;
+
+import com.asksven.betterbatterystats.R;
+import com.asksven.betterbatterystats.data.ReferenceStore;
+import com.asksven.betterbatterystats.data.StatsProvider;
+import com.asksven.betterbatterystats.services.WriteBootReferenceService;
+import com.asksven.betterbatterystats.services.WriteUnpluggedReferenceService;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * @author sven 
+ */
+public class FirstLaunch
+{
+
+	private static String TAG = "FirstLaunch";
+	
+	public static void app_launched(Activity ctx)
+	{
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+
+		boolean firstLaunch = !prefs.getBoolean("launched", false);
+
+		if (firstLaunch)
+		{
+			Log.i(TAG, "Application was launched for the first time: create 'unplugged' reference");
+			// Save that the app has been launched
+			SharedPreferences.Editor editor = prefs.edit();
+			editor.putBoolean("launched", true);
+			editor.commit();
+			
+			showInfoDialog(ctx);
+			
+			// start service to persist reference
+			Intent serviceIntent = new Intent(ctx, WriteUnpluggedReferenceService.class);
+			ctx.startService(serviceIntent);
+
+		}
+
+		
+	}
+
+	public static void showInfoDialog(final Activity ctx)
+	{		
+        AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+        builder.setMessage("A reference 'unplugged' is being created now as it is the first time the app is started. When you plug/unplug your phone this reference will be overwritten.")
+               .setCancelable(false)
+               .setTitle("Welcome to " + ctx.getString(R.string.app_name))
+               .setPositiveButton("OK", new DialogInterface.OnClickListener()
+               {
+                   public void onClick(DialogInterface dialog, int id)
+                   {		           
+                	   dialog.dismiss();
+                	   
+                	   // restart the app
+                	   Intent i = ctx.getBaseContext().getPackageManager()
+                	             .getLaunchIntentForPackage( ctx.getBaseContext().getPackageName() );
+                	i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                	ctx.startActivity(i);
+                   }
+               });
+        builder.create().show();
+	}
+}
