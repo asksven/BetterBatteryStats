a52b44dda1f5a2f51db154f3082920b0fc678107
asksven <sven.knispel@gmail.com>
2013-03-17 22:49:04 +0100

    Fixed - task 367: [feature] add dashclock support https://github.com/asksven/BetterBatteryStats/issues/issue/367

---
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/services/BbsDashClockExtension.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2011-2013 asksven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.asksven.betterbatterystats.services;
+
+import java.util.ArrayList;
+
+import com.asksven.android.common.privateapiproxies.BatteryStatsProxy;
+import com.asksven.android.common.privateapiproxies.Misc;
+import com.asksven.android.common.privateapiproxies.StatElement;
+import com.asksven.android.common.utils.DateUtils;
+import com.asksven.betterbatterystats.LogSettings;
+import com.asksven.betterbatterystats.PreferencesActivity;
+import com.asksven.betterbatterystats.R;
+import com.asksven.betterbatterystats.StatsActivity;
+import com.asksven.betterbatterystats.data.Reference;
+import com.asksven.betterbatterystats.data.ReferenceStore;
+import com.asksven.betterbatterystats.data.StatsProvider;
+import com.asksven.betterbatterystats.widgetproviders.SmallWidgetProvider;
+import com.asksven.betterbatterystats.widgets.WidgetBattery;
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+
+public class BbsDashClockExtension extends DashClockExtension
+{
+	private static final String TAG = "DashclockExtension";
+
+	public static final String PREF_NAME = "pref_name";
+
+	@Override
+	protected void onUpdateData(int reason)
+	{
+		// Get preference value.
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+		// we want to refresh out stats each time the screen goes on
+		setUpdateWhenScreenOn(true);
+		
+		// collect some data
+		String refFrom	= sharedPrefs.getString("small_widget_default_stat_type", Reference.UNPLUGGED_REF_FILENAME);
+
+		boolean showTitle	= sharedPrefs.getBoolean("widget_show_stat_type", true);
+
+		long timeAwake 		= 0;
+		long timeSince 		= 0;
+		long drain			= 0;
+		
+		String strAwake = "";
+		String strDrain = "";
+		
+		StatsProvider stats = StatsProvider.getInstance(this);
+		// make sure to flush cache
+		BatteryStatsProxy.getInstance(this).invalidate();
+
+		try
+		{
+			
+			Reference toRef = StatsProvider.getInstance(this).getUncachedPartialReference(0);
+			Reference fromRef = ReferenceStore.getReferenceByName(refFrom, this);
+
+
+			ArrayList<StatElement> otherStats = stats.getOtherUsageStatList(true, fromRef, false, true, toRef);
+			timeSince = stats.getSince(fromRef, toRef);
+			drain = stats.getBatteryLevelStat(fromRef, toRef);
+			
+			
+			if ( (otherStats == null) || ( otherStats.size() == 1) )
+			{
+				// the desired stat type is unavailable, pick the alternate one and go on with that one
+				refFrom	= sharedPrefs.getString("widget_fallback_stat_type", Reference.UNPLUGGED_REF_FILENAME);
+				fromRef = ReferenceStore.getReferenceByName(refFrom, this);
+				otherStats = stats.getOtherUsageStatList(true, fromRef, false, true, toRef);
+			}
+
+			if ( (otherStats != null) && ( otherStats.size() > 1) )
+			{
+
+				Misc timeAwakeStat = (Misc) stats.getElementByKey(otherStats, "Awake");
+				if (timeAwakeStat != null)
+				{
+					timeAwake = timeAwakeStat.getTimeOn();
+				}
+				else
+				{
+					timeAwake = 0;
+				}				
+			}
+		}
+		catch (Exception e)
+		{
+			Log.e(TAG, "Exception: "+Log.getStackTraceString(e));
+		}
+		finally
+		{
+			if (LogSettings.DEBUG)
+			{
+				Log.d(TAG, "Awake: " + DateUtils.formatDuration(timeAwake));
+				Log.d(TAG, "Since: " + DateUtils.formatDuration(timeSince));
+				Log.d(TAG, "Drain: " + drain);
+				
+			}
+
+			strAwake = DateUtils.formatDurationCompressed(timeAwake);
+			if (timeSince != 0)
+			{
+				strDrain = (drain / (timeSince/1000/60/60)) + "%/h";
+			}
+			else
+			{
+				strDrain = "0 %/h";
+			}
+		}
+
+		String refs = "Since " + Reference.getLabel(refFrom);
+		// Publish the extension data update.
+		publishUpdate(new ExtensionData().visible(true).icon(R.drawable.icon_notext).status(strDrain)
+				.expandedTitle(strAwake + " awake, " + strDrain).expandedBody(refs)
+				.clickIntent(new Intent(this, StatsActivity.class)));
+	}
+}
