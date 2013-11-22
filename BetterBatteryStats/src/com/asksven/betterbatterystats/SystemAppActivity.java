9cf1026285514925ced25fb947de38cdceb9b6a4
asksven <sven.knispel@gmail.com>
2013-11-22 11:49:32 +0100

    Fixed - task 430: [bug] blocking call when opening 'install as system app' and root is not granted https://github.com/asksven/BetterBatteryStats/issues/issue/430

--- BetterBatteryStats/src/com/asksven/betterbatterystats/SystemAppActivity.java
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/SystemAppActivity.java
@@ -9,6 +9,7 @@ import java.security.cert.X509Certificate;
 
 import javax.security.auth.x500.X500Principal;
 
+import com.asksven.android.common.utils.SysUtils;
 import com.asksven.android.common.utils.SystemAppInstaller;
 import com.asksven.android.common.utils.SystemAppInstaller.Status;
 import com.asksven.betterbatterystats.R;
@@ -18,10 +19,12 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -55,6 +58,37 @@ public class SystemAppActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_systemapp);
+
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+
+		boolean rootEnabled = sharedPrefs.getBoolean("root_features", false);
+
+		if ( !rootEnabled )
+		{
+			// show message that data is not available
+			// prepare the alert box
+            AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+ 
+            // set the message to display
+            alertbox.setMessage("In order to use this feature 'Root Features must be enabled (Advanced Preferences)");
+ 
+            // add a neutral button to the alert box and assign a click listener
+            alertbox.setNeutralButton("Ok", new DialogInterface.OnClickListener()
+            {
+ 
+                // click listener on the alert box
+                public void onClick(DialogInterface arg0, int arg1)
+                {
+                	setResult(RESULT_OK);
+                	finish();
+                }
+            });
+ 
+            // show it
+            alertbox.show();
+
+		}
 
 		// package name is either com.asksven.betterbatterystats or com.asksven.betterbatterystats_xdaedition 
 		String packageName = getPackageName();
