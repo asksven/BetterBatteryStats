68e152f43afdbba78ed4a94edd406709efff1ee6
asksven <sven.knispel@gmail.com>
2014-11-27 22:13:07 +0100

    Fixed #546

---
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/BbsApplication.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2014 asksven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asksven.betterbatterystats;
+
+import java.util.Locale;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.preference.PreferenceManager;
+
+
+/**
+ * @author android
+ *
+ */
+public class BbsApplication extends Application
+{
+
+	    private Locale localeEN = Locale.ENGLISH;
+
+	    @Override
+	    public void onConfigurationChanged(Configuration newConfig)
+	    {
+	        super.onConfigurationChanged(newConfig);
+	        
+	        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+	        boolean forceEN = settings.getBoolean("force_en", false);
+	        
+	        Configuration config = getBaseContext().getResources().getConfiguration();
+
+	        Locale appLocale = null;
+	        if (forceEN)
+	        {
+	        	appLocale = localeEN;
+	        }
+	        else
+	        {
+	        	appLocale = getResources().getConfiguration().locale;
+	        }
+            Locale.setDefault(appLocale);
+            config.locale = appLocale;
+            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+	    }
+
+	    @Override
+	    public void onCreate()
+	    {
+	        super.onCreate();
+
+	        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+	        boolean forceEN = settings.getBoolean("force_en", false);
+	        
+	        Configuration config = getBaseContext().getResources().getConfiguration();
+
+	        Locale appLocale = null;
+	        if (forceEN)
+	        {
+	        	appLocale = localeEN;
+	        }
+	        else
+	        {
+	        	appLocale = getResources().getConfiguration().locale;
+	        }
+            Locale.setDefault(appLocale);
+            config.locale = appLocale;
+            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+	    }
+}
