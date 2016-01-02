7f50fe75639129f013345eee64c6b8c708613f24
asksven <sven.knispel@gmail.com>
2016-01-02 12:37:23 +0100

    Make test button invisible in release APK (non debug)

--- BetterBatteryStats/src/com/asksven/betterbatterystats/AboutActivity.java
+++ BetterBatteryStats/src/com/asksven/betterbatterystats/AboutActivity.java
@@ -15,11 +15,22 @@
  */
 package com.asksven.betterbatterystats;
 
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.security.auth.x500.X500Principal;
+
 import com.asksven.betterbatterystats.R;
 import com.asksven.betterbatterystats.data.StatsProvider;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -90,14 +101,24 @@ public class AboutActivity extends BaseActivity
                 openURL(TWITTER_LINK);
             }
         });
+        
         final Button buttonTest = (Button) findViewById(R.id.buttonTest);
-        buttonTest.setOnClickListener(new View.OnClickListener()
+        if (isDebuggable(this))
         {
-            public void onClick(View v)
-            {
-                StatsProvider.getInstance(AboutActivity.this).testAPI();
-            }
-        });
+	        // show the test button only for non release stuff
+	        buttonTest.setOnClickListener(new View.OnClickListener()
+	        {
+	            public void onClick(View v)
+	            {
+	                StatsProvider.getInstance(AboutActivity.this).testAPI();
+	            }
+	        });
+        }
+        else
+        {
+        	buttonTest.setVisibility(View.GONE);
+        }
+        	
 
         
     }   
@@ -118,4 +139,37 @@ public class AboutActivity extends BaseActivity
         Intent intentCredits = new Intent(this, CreditsActivity.class);
         this.startActivity(intentCredits);
     }
+    
+	private static final X500Principal DEBUG_DN = new X500Principal("CN=Android Debug,O=Android,C=US");
+	private boolean isDebuggable(Context ctx)
+	{
+	    boolean debuggable = false;
+
+	    try
+	    {
+	        PackageInfo pinfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(),PackageManager.GET_SIGNATURES);
+	        Signature signatures[] = pinfo.signatures;
+
+	        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+	        for ( int i = 0; i < signatures.length;i++)
+	        {   
+	            ByteArrayInputStream stream = new ByteArrayInputStream(signatures[i].toByteArray());
+	            X509Certificate cert = (X509Certificate) cf.generateCertificate(stream);       
+	            debuggable = cert.getSubjectX500Principal().equals(DEBUG_DN);
+	            if (debuggable)
+	                break;
+	        }
+	    }
+	    catch (NameNotFoundException e)
+	    {
+	        //debuggable variable will remain false
+	    }
+	    catch (CertificateException e)
+	    {
+	        //debuggable variable will remain false
+	    }
+	    return debuggable;
+	}
+
 }
