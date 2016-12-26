b4158bdc663cfbd21cb57333db2f4caf8a61c743
asksven <sven.knispel@gmail.com>
2016-12-26 19:14:59 +0100

    Added instrumented tests

---
+++ app/src/androidTest/java/com/asksven/betterbatterystats/data/StatsProviderTest.java
@@ -0,0 +1,124 @@
+package com.asksven.betterbatterystats.data;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
+
+import com.asksven.android.common.privateapiproxies.StatElement;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+/**
+ * Created on 12/26/16.
+ */
+@RunWith(AndroidJUnit4.class)
+public class StatsProviderTest extends InstrumentationTestCase
+{
+	private StatsProvider mStats;
+	static final String TAG = "StatsProviderTest";
+
+	@Before
+	public void createStatsProvider() throws Exception
+	{
+		Context ctx = InstrumentationRegistry.getContext();
+		assertNotNull(ctx);
+		mStats = StatsProvider.getInstance(ctx);
+
+	}
+
+	@Test
+	public void test_getCurrentAlarmsStatList() throws Exception
+	{
+		ArrayList<StatElement> res = mStats.getCurrentAlarmsStatList(true);
+		assertNotNull(res);
+		Log.i(TAG, "Retrieved " + res.size() + " elements");
+	}
+
+	@Test
+	public void getCurrentSensorStatList() throws Exception
+	{
+		ArrayList<StatElement> res = mStats.getCurrentAlarmsStatList(true);
+		assertNotNull(res);
+		Log.i(TAG, "Retrieved " + res.size() + " elements");
+
+	}
+
+	@Test
+	public void getCurrentProcessStatList() throws Exception
+	{
+		ArrayList<StatElement> res = mStats.getCurrentProcessStatList(true, 0);
+		assertNotNull(res);
+		Log.i(TAG, "Retrieved " + res.size() + " elements");
+
+	}
+
+	@Test
+	public void getCurrentWakelockStatList() throws Exception
+	{
+		ArrayList<StatElement> res = mStats.getCurrentWakelockStatList(true, 0, 0);
+		assertNotNull(res);
+		Log.i(TAG, "Retrieved " + res.size() + " elements");
+
+	}
+
+	@Test
+	public void getCurrentKernelWakelockStatList() throws Exception
+	{
+		ArrayList<StatElement> res = mStats.getCurrentKernelWakelockStatList(true, 0, 0);
+		assertNotNull(res);
+		Log.i(TAG, "Retrieved " + res.size() + " elements");
+
+	}
+
+	@Test
+	public void getCurrentNetworkUsageStatList() throws Exception
+	{
+		ArrayList<StatElement> res = mStats.getCurrentNetworkUsageStatList(true);
+		assertNotNull(res);
+		Log.i(TAG, "Retrieved " + res.size() + " elements");
+
+	}
+
+	@Test
+	public void getCurrentCpuStateList() throws Exception
+	{
+		ArrayList<StatElement> res = mStats.getCurrentCpuStateList(true);
+		assertNotNull(res);
+		Log.i(TAG, "Retrieved " + res.size() + " elements");
+
+	}
+
+	@Test
+	public void getRequestedPermissionListForPackage() throws Exception
+	{
+
+	}
+
+	@Test
+	public void getReceiverListForPackage() throws Exception
+	{
+
+	}
+
+	@Test
+	public void getServiceListForPackage() throws Exception
+	{
+
+	}
+
+	@Test
+	public void getCurrentOtherUsageStatList() throws Exception
+	{
+		ArrayList<StatElement> res = mStats.getCurrentOtherUsageStatList(true, true, false);
+		assertNotNull(res);
+		Log.i(TAG, "Retrieved " + res.size() + " elements");
+
+	}
+
+}
