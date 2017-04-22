563e32a328ca3c25fda4d693549522fe62be3d98
asksven <sven.knispel@gmail.com>
2017-04-22 12:34:49 +0200

    Fixed decypting keystores

--- circleciscripts/download_keystore.sh
+++ circleciscripts/download_keystore.sh
@@ -2,6 +2,6 @@
 curl -O ${KEYSTORE_URI}/${KEYSTORE_RELEASE}-cypher
 curl -O ${KEYSTORE_URI}/${KEYSTORE_DEBUG}-cypher
 
-if [ -e ${KEYSTORE_RELEASE} ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_RELEASE; fi
-if [ -e ${KEYSTORE_DEBUG} ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_DEBUG; fi
+if [ -e ${KEYSTORE_RELEASE}-cypher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_RELEASE; fi
+if [ -e ${KEYSTORE_DEBUG}-cypher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_DEBUG; fi
 
