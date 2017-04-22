2af1d330ca2586145adec61065ebcf9687d92fc2
asksven <sven.knispel@gmail.com>
2017-04-22 14:00:16 +0200

    Fixed renaming

--- circleciscripts/renameapks.sh
+++ circleciscripts/renameapks.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-for filename in find $HOME/BetterBatteryStats/app/build/outputs/apk -name "*.apk"; do rename -v 's/(.*)\.apk/$1-$ENV{'CIRCLE_BUILD_NUM'}.apk/' $filename; done;
+#!/usr/bin/env bash
+
+for filename in $(find $HOME/BetterBatteryStats/app/build/outputs/apk -name "*.apk"); do rename -v 's/(.*)\.apk/$1-$ENV{"CIRCLE_BUILD_NUM"}.apk/' $filename; done;
