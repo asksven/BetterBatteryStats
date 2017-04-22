a4b9fc5dbd0a49d113ce4cd20ca7447ca1e5222c
asksven <sven.knispel@gmail.com>
2017-04-22 11:34:30 +0200

    Fixed

--- circleciscripts/download_keystore.sh
+++ circleciscripts/download_keystore.sh
@@ -1,21 +1,7 @@
-# use curl to download a keystore from $KEYSTORE_URI, if set,
-# to the path/filename set in $KEYSTORE_RELEASE and/or $KEYSTORE_DEBUG.
+#!/bin/bash
+curl -O ${KEYSTORE_URI}/${KEYSTORE_RELEASE}-cypher
+curl -O ${KEYSTORE_URI}/${KEYSTORE_DEBUG}-cypher
 
-if [[ $KEYSTORE_RELEASE && ${KEYSTORE_RELEASE} && $KEYSTORE_URI && ${KEYSTORE_URI} ]]
-then
-    echo "Keystore detected - downloading..."
-    curl -L -o ${KEYSTORE_RELEASE}-cypher ${KEYSTORE_URI}
-else
-    echo "Keystore uri not set.  .APK artifact will not be signed."
-fi
+if [ -e ${KEYSTORE_RELEASE} ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $KEYSTORE_RELEASE; fi
+if [ -e ${KEYSTORE_DEBUG} ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $KEYSTORE_DEBUG; fi
 
-if [[ $KEYSTORE_DEBUG && ${KEYSTORE_DEBUG} && $KEYSTORE_URI && ${KEYSTORE_URI} ]]
-then
-    echo "Keystore detected - downloading..."
-    curl -L -o ${KEYSTORE_DEBUG}-cypher ${KEYSTORE_URI}
-else
-    echo "Keystore uri not set.  .APK artifact will not be signed."
-fi
-
-if [ -e ${KEYSTORE_RELEASE} ] then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $(KEYSTORE_RELEASE}; fi;
-if [ -e ${KEYSTORE_DEBUG} ] then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $(KEYSTORE_DEBUG}; fi;
