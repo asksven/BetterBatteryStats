040da2a11269ad33c77751fc2e755b45c931fa9f
asksven <sven.knispel@gmail.com>
2017-04-22 12:48:26 +0200

    Typo

--- circleciscripts/download_keystore.sh
+++ circleciscripts/download_keystore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -O ${KEYSTORE_URI}/${KEYSTORE_RELEASE}-cypher
-curl -O ${KEYSTORE_URI}/${KEYSTORE_DEBUG}-cypher
+curl -O ${KEYSTORE_URI}/${KEYSTORE_RELEASE}-cipher
+curl -O ${KEYSTORE_URI}/${KEYSTORE_DEBUG}-cipher
 
-if [ -e ${KEYSTORE_RELEASE}-cypher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_RELEASE; fi
-if [ -e ${KEYSTORE_DEBUG}-cypher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_DEBUG; fi
+if [ -e ${KEYSTORE_RELEASE}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_RELEASE; fi
+if [ -e ${KEYSTORE_DEBUG}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_DEBUG; fi
 
