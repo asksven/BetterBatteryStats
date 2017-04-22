64160309238dfd67559c2151ce43333a28d118e2
asksven <sven.knispel@gmail.com>
2017-04-22 12:19:23 +0200

    Fixed perl expression to use env var

--- circleciscripts/renameapks.sh
+++ circleciscripts/renameapks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for filename in find $HOME/BetterBatteryStats/app/build/outputs/apk -name "*.apk"; do rename 's/(.*)\.apk/$1-$CIRCLE_BUILD_NUM.apk/' $filename; done;
+for filename in find $HOME/BetterBatteryStats/app/build/outputs/apk -name "*.apk"; do rename -v 's/(.*)\.apk/$1-$ENV{'CIRCLE_BUILD_NUM'}.apk/' $filename; done;
