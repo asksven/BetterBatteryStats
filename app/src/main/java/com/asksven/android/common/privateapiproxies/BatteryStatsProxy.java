33894f42207db653536f068003b46fb60e079c57
asksven <sven.knispel@gmail.com>
2017-05-01 20:14:46 +0200

    Fixed #790: possible error cause for the battery_stats service not responding

--- app/src/main/java/com/asksven/android/common/privateapiproxies/BatteryStatsProxy.java
+++ app/src/main/java/com/asksven/android/common/privateapiproxies/BatteryStatsProxy.java
@@ -17,6 +17,8 @@
 package com.asksven.android.common.privateapiproxies;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -38,7 +40,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.MemoryFile;
 import android.os.Parcel;
+import android.os.ParcelFileDescriptor;
 import android.os.Parcelable;
 import android.os.SystemClock;
 import android.util.ArrayMap;
@@ -99,13 +103,30 @@ public class BatteryStatsProxy
 	{
 		if (m_proxy == null)
 		{
-			m_proxy = new BatteryStatsProxy(ctx);
+            if (Build.VERSION.SDK_INT >= 22)
+            {
+                m_proxy = new BatteryStatsProxy(ctx, true);
+            }
+            else
+            {
+                m_proxy = new BatteryStatsProxy(ctx);
+            }
 		}
 		
 		return m_proxy;
 	}
-	
-	public void invalidate()
+
+    synchronized public static BatteryStatsProxy getInstanceNew(Context ctx)
+    {
+        if (m_proxy == null)
+        {
+            m_proxy = new BatteryStatsProxy(ctx, true); // use new method
+        }
+
+        return m_proxy;
+    }
+
+    public void invalidate()
 	{
 		m_proxy = null;
 	}
@@ -113,7 +134,7 @@ public class BatteryStatsProxy
     /**
 	 * Default cctor
 	 */
-	private BatteryStatsProxy(Context context) 
+	protected BatteryStatsProxy(Context context)
 	{
 		/*
 		 * As BatteryStats is a service we need to get a binding using the IBatteryStats.Stub.getStatistics()
@@ -263,7 +284,196 @@ public class BatteryStatsProxy
 	    	
 	    }    
 	}
-	
+
+	protected BatteryStatsProxy(Context context, boolean value)
+	{
+		/*
+		 * As BatteryStats is a service we need to get a binding using the IBatteryStats.Stub.getStatistics()
+		 * method (using reflection).
+		 * If we would be using a public API the code would look like:
+		 * @see com.android.settings.fuelgauge.PowerUsageSummary.java
+		 * protected void onCreate(Bundle icicle) {
+         *  super.onCreate(icicle);
+		 *
+         *  mStats = (BatteryStatsImpl)getLastNonConfigurationInstance();
+		 *
+         *  addPreferencesFromResource(R.xml.power_usage_summary);
+         *  mBatteryInfo = IBatteryStats.Stub.asInterface(
+         *       ServiceManager.getService("batteryinfo"));
+         *  mAppListGroup = (PreferenceGroup) findPreference("app_list");
+         *  mPowerProfile = new PowerProfile(this);
+    	 * }
+		 *
+		 * followed by
+		 * private void load() {
+         *	try {
+         *   byte[] data = mBatteryInfo.getStatistics();
+         *   Parcel parcel = Parcel.obtain();
+         *   parcel.unmarshall(data, 0, data.length);
+         *   parcel.setDataPosition(0);
+         *   mStats = com.android.internal.os.BatteryStatsImpl.CREATOR
+         *           .createFromParcel(parcel);
+         *   mStats.distributeWorkLocked(BatteryStats.STATS_SINCE_CHARGED);
+         *  } catch (RemoteException e) {
+         *   Log.e(TAG, "RemoteException:", e);
+         *  }
+         * }
+		 */
+
+		try
+		{
+			ClassLoader cl = context.getClassLoader();
+
+			m_ClassDefinition = cl.loadClass("com.android.internal.os.BatteryStatsImpl");
+
+			// get the IBinder to the "batteryinfo" service
+			@SuppressWarnings("rawtypes")
+			Class serviceManagerClass = cl.loadClass("android.os.ServiceManager");
+
+			// parameter types
+			@SuppressWarnings("rawtypes")
+			Class[] paramTypesGetService= new Class[1];
+			paramTypesGetService[0]= String.class;
+
+			@SuppressWarnings("unchecked")
+			Method methodGetService = serviceManagerClass.getMethod("getService", paramTypesGetService);
+
+			String service = "";
+			if (Build.VERSION.SDK_INT >= 19)
+			{
+				// kitkat and following
+				service = "batterystats";
+			}
+			else
+			{
+				service = "batteryinfo";
+			}
+			// parameters
+			Object[] paramsGetService= new Object[1];
+			paramsGetService[0] = service;
+
+			if (CommonLogSettings.DEBUG)
+			{
+				Log.i(TAG, "invoking android.os.ServiceManager.getService(\"batteryinfo\")");
+			}
+			IBinder serviceBinder = (IBinder) methodGetService.invoke(serviceManagerClass, paramsGetService);
+
+			if (CommonLogSettings.DEBUG)
+			{
+				Log.i(TAG, "android.os.ServiceManager.getService(\"batteryinfo\") returned a service binder");
+			}
+
+			// now we have a binder. Let's us that on IBatteryStats.Stub.asInterface
+			// to get an IBatteryStats
+			// Note the $-syntax here as Stub is a nested class
+			@SuppressWarnings("rawtypes")
+			Class iBatteryStatsStub = cl.loadClass("com.android.internal.app.IBatteryStats$Stub");
+
+			//Parameters Types
+			@SuppressWarnings("rawtypes")
+			Class[] paramTypesAsInterface= new Class[1];
+			paramTypesAsInterface[0]= IBinder.class;
+
+			@SuppressWarnings("unchecked")
+			Method methodAsInterface = iBatteryStatsStub.getMethod("asInterface", paramTypesAsInterface);
+
+			// Parameters
+			Object[] paramsAsInterface= new Object[1];
+			paramsAsInterface[0] = serviceBinder;
+
+			if (CommonLogSettings.DEBUG)
+			{
+				Log.i(TAG, "invoking com.android.internal.app.IBatteryStats$Stub.asInterface");
+			}
+			Object iBatteryStatsInstance = methodAsInterface.invoke(iBatteryStatsStub, paramsAsInterface);
+
+			// and finally we call getStatistics from that IBatteryStats to obtain a Parcel
+			@SuppressWarnings("rawtypes")
+			Class iBatteryStats = cl.loadClass("com.android.internal.app.IBatteryStats");
+
+			@SuppressWarnings("unchecked")
+			Method methodGetStatistics = iBatteryStats.getMethod("getStatistics");
+
+            @SuppressWarnings("unchecked")
+            Method methodGetStatisticsStream = iBatteryStats.getMethod("getStatisticsStream");
+            // returns a ParcelFileDescriptor
+
+            if (CommonLogSettings.DEBUG)
+			{
+				Log.i(TAG, "invoking getStatisticsStream");
+			}
+            ParcelFileDescriptor pfd = (ParcelFileDescriptor) methodGetStatisticsStream.invoke(iBatteryStatsInstance);
+
+
+            if (pfd != null)
+            {
+                FileInputStream fis = new ParcelFileDescriptor.AutoCloseInputStream(pfd);
+                try
+                {
+                    if (CommonLogSettings.DEBUG) { Log.i(TAG, "retrieving parcel"); }
+
+                    byte[] data = readFully(fis);
+                    Parcel parcel = Parcel.obtain();
+                    parcel.unmarshall(data, 0, data.length);
+                    parcel.setDataPosition(0);
+
+                    @SuppressWarnings("rawtypes")
+                    Class batteryStatsImpl = cl.loadClass("com.android.internal.os.BatteryStatsImpl");
+
+                    if (CommonLogSettings.DEBUG) { Log.i(TAG, "reading CREATOR field"); }
+                    Field creatorField = batteryStatsImpl.getField("CREATOR");
+
+                    // From here on we don't need reflection anymore
+                    @SuppressWarnings("rawtypes")
+                    Parcelable.Creator batteryStatsImpl_CREATOR = (Parcelable.Creator) creatorField.get(batteryStatsImpl);
+
+                    m_Instance = batteryStatsImpl_CREATOR.createFromParcel(parcel);
+
+                }
+                catch (IOException e)
+                {
+                    Log.w(TAG, "Unable to read statistics stream", e);
+                }
+            }
+
+		}
+		catch( Exception e )
+		{
+			if (e instanceof InvocationTargetException && e.getCause() != null) {
+				Log.e(TAG, "An exception occured in BatteryStatsProxy(). Message: " + e.getCause().getMessage());
+			} else {
+				Log.e(TAG, "An exception occured in BatteryStatsProxy(). Message: " + e.getMessage());
+			}
+			m_Instance = null;
+
+		}
+	}
+
+	// both these methods come from com/android/internal/os/BatteryStatsHelper.java
+    protected static byte[] readFully(FileInputStream stream) throws java.io.IOException {
+        return readFully(stream, stream.available());
+    }
+    protected static byte[] readFully(FileInputStream stream, int avail) throws java.io.IOException {
+        int pos = 0;
+        byte[] data = new byte[avail];
+        while (true) {
+            int amt = stream.read(data, pos, data.length-pos);
+            //Log.i("foo", "Read " + amt + " bytes at " + pos
+            //        + " of avail " + data.length);
+            if (amt <= 0) {
+                //Log.i("foo", "**** FINISHED READING: pos=" + pos
+                //        + " len=" + data.length);
+                return data;
+            }
+            pos += amt;
+            avail = stream.available();
+            if (avail > data.length-pos) {
+                byte[] newData = new byte[pos+avail];
+                System.arraycopy(data, 0, newData, 0, pos);
+                data = newData;
+            }
+        }
+    }
 	/**
 	 * Returns true if the proxy could not be initialized properly
 	 * @return true if the proxy wasn't initialized
