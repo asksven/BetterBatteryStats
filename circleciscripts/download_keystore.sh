96c313c85141520cecda59de16ac51e803aaae60
asksven <sven.knispel@gmail.com>
2017-11-26 18:47:59 +0100

    Moved to circleci v2 Adapted all scripts Moved some env vars from circleci to managed encrypted file

--- circleciscripts/download_keystore.sh
+++ circleciscripts/download_keystore.sh
@@ -2,6 +2,6 @@
 curl -O ${KEYSTORE_URI}/${KEYSTORE_RELEASE}-cipher
 curl -O ${KEYSTORE_URI}/${KEYSTORE_DEBUG}-cipher
 
-if [ -e ${KEYSTORE_RELEASE}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_RELEASE; fi
-if [ -e ${KEYSTORE_DEBUG}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $HOME/BetterBatteryStats/app/$KEYSTORE_DEBUG; fi
+if [ -e ${KEYSTORE_RELEASE}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> ./app/$KEYSTORE_RELEASE; fi
+if [ -e ${KEYSTORE_DEBUG}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> ./app/$KEYSTORE_DEBUG; fi
 
