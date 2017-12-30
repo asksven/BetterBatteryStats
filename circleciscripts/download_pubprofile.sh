676b15f3531bf6e2287b38ffa1856316ef5facdb
asksven <sven.knispel@gmail.com>
2017-12-30 18:55:53 +0100

    Fixed gplay publishing profile

--- circleciscripts/download_pubprofile.sh
+++ circleciscripts/download_pubprofile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -O ${KEYSTORE_URI}/sa-google-play.json-cipher
 
-if [ -e sa-google-play.json-cipher ]; then openssl aes-256-cbc -d -in sa-google-play.json-cipher -k $KEY >> $HOME/BetterBatteryStats/app/sa-google-play.json; fi
+if [ -e sa-google-play.json-cipher ]; then openssl aes-256-cbc -d -in sa-google-play.json-cipher -k $KEY >> ./app/sa-google-play.json; fi
 
