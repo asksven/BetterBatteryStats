ed10891521b2c78cc3e14e85b6f59c6c73864bf8
Taco <SkytkRSfan3895@gmail.com>
2018-12-17 21:54:01 -0500

    rearrange and simplify build.gradle files, bump some dependencies and play-publisher, simple variable usage in .sh files, etc
    Signed-off-by: Taco <SkytkRSfan3895@gmail.com>

--- circleciscripts/renameapks.sh
+++ circleciscripts/renameapks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-for filename in $(find $HOME/BetterBatteryStats/app/build/outputs/apk -name "*.apk"); do rename -v 's/(.*)\.apk/$1-$ENV{"CIRCLE_BUILD_NUM"}.apk/' $filename; done;
+for filename in $(find $HOME/BetterBatteryStats/app/build/outputs/apk -name "*.apk"); do rename -v 's/(.*)\.apk/$1-$ENV{"CIRCLE_BUILD_NUM"}.apk/' ${filename}; done;
