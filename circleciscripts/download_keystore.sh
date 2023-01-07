c906c2d2d915e63f3aa3ad665b722c1e96c86979
Sven Knispel <sven.knispel@pobox.com>
2023-01-07 16:16:42 +0100

    Added some logging

--- circleciscripts/download_keystore.sh
+++ circleciscripts/download_keystore.sh
@@ -3,3 +3,7 @@
 if [ -e ${KEYSTORE_RELEASE}-cipher ]; then openssl enc -in ${KEYSTORE_RELEASE}-cipher -out ./app/$KEYSTORE_RELEASE -k $KEY; fi
 if [ -e ${KEYSTORE_DEBUG}-cipher ]; then openssl enc -in ${KEYSTORE_DEBUG}-cipher -out ./app/$KEYSTORE_DEBUG -k $KEY; fi
 
+ls -la ${KEYSTORE_RELEASE}*
+ls -la ${KEYSTORE_DEBUG}*
+
+
